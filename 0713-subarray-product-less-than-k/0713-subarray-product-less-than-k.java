56c13cc52c3ac03cc6a251894c20b815c9390fcb
Pravin Rathod <pr819270@gmail.com>
2026-07-08 09:57:38 +0530

    Time: 3 ms (93.32%), Space: 48 MB (98.38%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,22 +1,30 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
 
-        if (k <= 1) return 0;
+        // If k is 0 or 1, no positive product can be less than k
+        if (k <= 1) {
+            return 0;
+        }
 
-        int left = 0;
-        int product = 1;
-        int count = 0;
+        int left = 0;          // Left pointer of the sliding window
+        long product = 1;      // Stores the product of the current window
+        int count = 0;         // Stores the total number of valid subarrays
 
+        // Expand the window by moving the right pointer
         for (int right = 0; right < nums.length; right++) {
 
+            // Include the current element in the product
             product *= nums[right];
 
+            // Shrink the window until the product becomes less than k
             while (product >= k) {
                 product /= nums[left];
                 left++;
             }
 
-            count += right - left + 1;
+            // All subarrays ending at 'right' and starting
+            // from 'left' to 'right' are valid
+            count += (right - left + 1);
         }
 
         return count;
